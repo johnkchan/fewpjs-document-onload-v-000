800b1fd42fb8266b247f646ceb3df064abeb8cd8
Jonathan Chan <johnkchan252@gmail.com>
2020-01-25 15:26:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
-  let element = docuement.querySelector("#text");
+  let element = document.querySelector("#text");
   
   element.innerHTML = "This is really cool!";
 });
